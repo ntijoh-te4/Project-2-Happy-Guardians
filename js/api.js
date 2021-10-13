9ea223c691fbd7d6bd27b1b34f8fdc868e84023a
nikki-andersson-nti-johanneberg <56922646+Chiyamii@users.noreply.github.com>
2021-10-13 13:58:45 +0200

    Removed loopRepos

--- js/api.js
+++ js/api.js
@@ -15,24 +15,4 @@ async function getRepositories(user) {
         .then(data => data)
 }
 
-async function loopResult() {
-    let result;
-    await getRepositories("olantig").then((res) => result = res)       
-    const template = document.querySelector(".repos");
-    const box = document.querySelector(".row")
-    result.forEach(element => {
-        const clone = template.content.cloneNode(true);
-        box.content = '';
-        clone.querySelector(".card-title").textContent = element.name;
-        clone.querySelector(".fork-count").textContent = element.forks_count;
-        console.log(element);
-        box.appendChild(clone);
-    });
-    return result;
-}
-
 console.log("connected")
-
-console.log(getRepositories("ntijoh-nikki-andersson"));
-
-console.log(loopResult());
