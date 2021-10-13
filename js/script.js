53b1c608efa805d053278182e8708f01242f6cd4
nikki-andersson-nti-johanneberg <56922646+Chiyamii@users.noreply.github.com>
2021-10-13 15:24:29 +0200

    gjort så sökfunktionen fungerar och gjort så man ser forks semi dynamiskt

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-var promise = import("./api.js");
+import("./api.js");
 const box = document.querySelector(".row")
+const input = document.querySelector("input")
 
 window.onload = (event) => {
     const template = document.querySelector('.index')
@@ -7,15 +8,38 @@ window.onload = (event) => {
     box.append(clone);
 };
 
-async function loopRepos() {
+async function loopRepos(username) {
     let result;
-    await getRepositories("olantig").then((res) => result = res)       
+    await getRepositories(username).then((res) => result = res)       
     const template = document.querySelector(".repos");
     result.forEach(element => {
         const clone = template.content.cloneNode(true);
         clone.querySelector(".card-title").textContent = element.name;
         clone.querySelector(".fork-count").textContent = element.forks_count;
+        clone.querySelector(".git-link").href = element.clone_url;
         console.log(element);
         box.appendChild(clone);
     });
 }
+
+async function loopForks() {
+    let result;
+    await getForks("olantig", "wu1920-projekt2").then((res) => result = res);   
+    const template = document.querySelector(".forks");
+    result.forEach(element => {
+        const clone = template.content.cloneNode(true);
+        clone.querySelector(".card-title").textContent = element.full_name;
+        clone.querySelector(".git-link").href = element.clone_url;
+        console.log(element);
+        box.appendChild(clone);
+    });  
+}
+
+input.addEventListener('input', (e) => {
+    box.innerHTML = '';
+    loopRepos(input.value)
+});
+
+box.addEventListener('click', (e) => {
+
+});
